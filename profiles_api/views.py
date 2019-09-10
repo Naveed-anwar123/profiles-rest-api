02dac444e905f367e2557d99d2969e0972c40192
Naveed Anwar <naveedanwar152@gmail.com>
2019-09-10 16:59:16 +0500

    Added APIView and configured urls

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return a list of APIViews"""
+        an_api_view = [
+            'APIViews functions are (get, post, put, patch)',
+            'It is similar to traditional django view',
+            'Gives you most control over application login',
+            'Is mapped to urls manually'
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_api_view})
